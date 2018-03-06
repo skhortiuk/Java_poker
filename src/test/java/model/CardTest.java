ccf724211236828cc1c4849df193732c2a5e25c4
mrluck <mrlcuk707@gmail.com>
2018-03-07 01:37:03 +0200

    Add test

---
+++ src/test/java/model/CardTest.java
@@ -0,0 +1,28 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+    Card card;
+
+    @BeforeEach
+    void setUp() {
+        card = new Card(2,2);
+    }
+
+    @Test
+    void getNameValue() {
+        assertEquals("Two", card.getNameValue());
+    }
+
+    @Test
+    void getSuitValue() {
+    }
+
+    @Test
+    void getValue() {
+    }
+}
