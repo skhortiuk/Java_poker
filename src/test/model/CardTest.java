8af22203bf0facfbb4a36a7f89a526bf9fdc576d
mrluck <mrlcuk707@gmail.com>
2018-03-15 12:51:24 +0200

    Junit4

---
+++ src/test/model/CardTest.java
@@ -0,0 +1,29 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Card;
+
+import static org.junit.Assert.*;
+
+public class CardTest {
+
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getNameValue() {
+    }
+
+    @Test
+    public void getSuitValue() {
+    }
+
+    @Test
+    public void getValue() {
+    }
+}
