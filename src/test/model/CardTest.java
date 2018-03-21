08fc9dc600cbc5a21be0126a8e01b8c1c262a823
mrluck <mrlcuk707@gmail.com>
2018-03-21 10:04:05 +0200

    Change test

--- src/test/model/CardTest.java
+++ src/test/model/CardTest.java
@@ -1,33 +1,14 @@
 package model;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import model.Card;
 
 import static org.junit.Assert.*;
 
 public class CardTest {
 
-    /*dasd*/
-
-    private Card card;
-
-    @Before
-    public void setUp() throws Exception {
-        card = new Card(2, 2);
-    }
-
-    @Test
-    public void getNameValue() {
-    }
-
-    @Test
-    public void getSuitValue() {
-    }
-
     @Test
     public void getValue() {
+        Card card = new Card(2, 2);
         assertEquals(2, card.getValue());
     }
 }
