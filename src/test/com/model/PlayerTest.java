101510c3cbe14d9bb3a0db8154c82c90a1bbfadd
zloyprogramist <juraharun333@gmail.com>
2018-04-26 22:31:26 +0300

    duplicate

--- src/test/com/model/PlayerTest.java
+++ src/test/com/model/PlayerTest.java
@@ -1,87 +1,46 @@
 package com.model;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
-class PlayerTest {
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PlayerTest {
 
     Player player = new Player();
 
-    @Test
-    void isWinner() {
-        boolean actual = player.isWinner();
-        assertFalse(actual);
-    }
 
-
-    @Test
-    void isInGame() {
-        boolean actual = player.isInGame();
-        assertTrue(actual);
-    }
-
-
-    @Test
-    void isAllIn() {
-        boolean actual = player.isAllIn();
-        assertFalse(actual);
-    }
-
-    @Test
-    void setAllIn() {
-    }
-
-    @Test
-    void getChips() {
-    }
-
-    @Test
-    void setChips() {
-    }
-
-    @Test
-    void getCard() {
-    }
-
-    @Test
-    void getCards() {
-    }
-
-    @Test
-    void addStack() {
-    }
-
-    @Test
-    void getStack() {
-    }
-
-    @Test
-    void clearChips() {
-    }
-
-    @Test
-    void getStackValue() {
-    }
-
-    @Test
-    void getPower() {
-    }
-
-    @Test
-    void getCombinationName() {
-    }
-
-    @Test
-    void del() {
-
-    }
-
-    @Test
-    void getHandCard() {
-    }
-
-    @Test
-    void setCards() {
-    }
+//    @Test
+//    public void addStack() {
+//
+//    }
+//
+//    @Test
+//    public void getStack() {
+//    }
+//
+//    @Test
+//    public void clearChips() {
+//
+//    }
+//
+//
+//    @Test
+//    public void getPower() {
+//    }
+//
+//
+//    @Test
+//    public void del() {
+//
+//
+//    }
+//
+//
+//    @Test
+//    public void setCards() {
+//    }
 }
