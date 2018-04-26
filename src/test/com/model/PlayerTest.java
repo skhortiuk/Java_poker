54869d279afdd383bd02091d137014e5043c1790
zloyprogramist <juraharun333@gmail.com>
2018-04-26 22:52:06 +0300

    revers

--- src/test/com/model/PlayerTest.java
+++ src/test/com/model/PlayerTest.java
@@ -1,46 +1,5 @@
-package com.model;
-
-import org.junit.Test;
-
-import java.util.ArrayList;
-
 import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlayerTest {
 
-    Player player = new Player();
-
-
-    @Test
-    public void addStack() {
-
-    }
-
-    @Test
-    public void getStack() {
-    }
-
-    @Test
-    public void clearChips() {
-
-    }
-
-
-    @Test
-    public void getPower() {
-    }
-
-
-    @Test
-    public void del() {
-
-
-    }
-
-
-    @Test
-    public void setCards() {
-    }
 }
