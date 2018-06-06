e539a59ce2a831aa2aafcb16f4097df38baf58fc
MrLuckUA <mrluck707@gmail.com>
2018-06-06 11:18:40 +0300

    refactor test combinations

--- src/test/com/model/CombinationsTest.java
+++ src/test/com/model/CombinationsTest.java
@@ -149,7 +149,7 @@ public class CombinationsTest {
         combinations.calculate();
         combinations.calculatePower();
         assertEquals(95.0, combinations.getPower(), 0.0);
-        assertEquals(new Card(14, 4).getValue(), 14;
+        assertEquals(new Card(14, 4).getValue(), 14);
         assertEquals("Straight to FIVE", combinations.getCombinationName());
     }
 
